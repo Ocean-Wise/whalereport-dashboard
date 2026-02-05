dd5d79f34012ffc9008410bfa53625f9f35a84a8
Alex Mitchell <alextommitchell@gmail.com>
2026-02-05 19:54:14 +0000

    keep source in df

--- data-requests-by-carly/NOAA_hb_sightings_01_30_2026.R
+++ data-requests-by-carly/NOAA_hb_sightings_01_30_2026.R
@@ -99,7 +99,6 @@ hb_not_processed = hb_sightings %>%
   dplyr::filter(!status %in% c("approved", "auto_approved"))
   
   
- 
 
 hb_sightings %>%
   dplyr::distinct(sighting_date) %>%
@@ -119,7 +118,7 @@ hb_sightings %>%
 library(leaflet)
 library(sf)
 
-leaflet::leaflet() %>% 
+leaflet() %>% 
   leaflet::addProviderTiles(providers$OpenStreetMap) %>% 
   leaflet::addPolygons(
     data = box_polynew,
