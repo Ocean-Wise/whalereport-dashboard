95e3a42bbf711420f5b3ab7dadf4eb3f62c7b6e3
Alex Mitchell <alextommitchell@gmail.com>
2026-03-26 14:42:43 +0000

    updates to prpa request data

--- data-requests-by-carly/Ashley Data Request NC PRPA - 03-05-2026.R
+++ data-requests-by-carly/Ashley Data Request NC PRPA - 03-05-2026.R
@@ -7,58 +7,72 @@
 ##run Config R, Data-import R, Data-cleaning R
 ##using the sightings_main table as it contains all sightings, not just sightings that sent alerts
 
+
+###### VISUALS REQUIRED
+# All sightings, aggregated from all sources [/]
+# All alerts, aggregated from all sources [/]
+# Species breakdown, all sources [/]
+# Map of sightings - OWSN and Whale Alert Alaska only [/]
+# Map of alerts sent - all sources, based on where alert was sent [/] <<<< BASED ON SIGHTING AS NO DATA FROM 2024 ON ALERT LOCATION
+# Breakdown of alert types (% of proximity vs zone of interest) [/]
+#######
+
+
 ##load the North Coast shapefile
-northcoast = sf::st_read("C:/Users/CarlyGreen/OneDrive - Ocean Wise Conservation Association/Documents/Operations/RStudio/Data Requests/North Coast Requests/North Coast Data Pull.shp")
+northcoast = sf::st_read("/Users/alexmitchell/Downloads/nc-area/North Coast Data Pull.shp")
 
 sf::st_crs(northcoast) ##no need to transform 
 
-#step 1 create start and end dates 
-start_date = lubridate::as_date("2025-01-01")
-end_date = lubridate::as_date("2025-12-31")
+# start / end filter with PST
+start_date = lubridate::ymd_hms("2025-01-01 00:00:00", tz = "America/Los_Angeles")
+end_date   = lubridate::ymd_hms("2025-12-31 23:59:59", tz = "America/Los_Angeles")
 
-
-##step 2 filter sightings data
+# filter and clean sightings
 nc_sightings = sightings_main %>% 
+  sf::st_as_sf(coords = c("report_longitude", "report_latitude"), crs = 4326, remove = FALSE) %>%
+  sf::st_filter(northcoast) %>%
+  sf::st_drop_geometry() %>% 
+  
+  dplyr::mutate(
+    # Convert everything to PST/PDT consistently
+    sighting_date = lubridate::with_tz(sighting_date, tzone = "America/Los_Angeles"),
+    lat_rnd = round(report_latitude, 4),
+    lon_rnd = round(report_longitude, 4),
+    time_bucket = lubridate::round_date(sighting_date, "15 mins"),
+  ) %>%
   dplyr::filter(
     sighting_date >= start_date,
-    sighting_date <= end_date) %>% 
-  dplyr::mutate(
-  sighting_date = dplyr::case_when(
-    stringr::str_detect(comments,"Historical Import") == T ~ lubridate::force_tz(sighting_date, tzone = "America/Los_Angeles"),
-    TRUE ~ lubridate::with_tz(sighting_date, tzone = "America/Los_Angeles")
-  )) %>%
-  dplyr::group_by(sighting_date, report_latitude, report_longitude, observer_email) %>% ##trying to group and remove duplicates
-  dplyr::mutate(
-    is_duplicate = dplyr::n() > 1
-  ) %>%
-  dplyr::slice(1) %>%
-  dplyr::ungroup() %>% 
-  dplyr::filter(report_source_entity== "Ocean Wise Conservation Association") %>% 
-  sf::st_as_sf(coords = c("report_longitude", "report_latitude"), crs = 4326, remove = FALSE) %>% 
-  sf::st_filter(northcoast) %>% 
-  dplyr::mutate(observer_email_clean = tolower(observer_email)) %>% 
-  dplyr::group_by(report_longitude, observer_email_clean) %>% ##continuing to remove duplicates that remain with slightly diff dates
+    sighting_date <= end_date
+  ) %>% 
+  dplyr::arrange(sighting_date) %>% 
+  # REMOVED observer_email from grouping to catch NA mismatches
+  dplyr::group_by(lat_rnd, lon_rnd, time_bucket) %>% 
   dplyr::mutate(
     is_duplicate = dplyr::n() > 1
   ) %>% 
   dplyr::slice(1) %>% 
-  dplyr::ungroup() %>% 
-  sf::st_drop_geometry()
+  dplyr::ungroup()
 
 
 ##step 3 check for duplicates 
 nc_sightings %>%
   dplyr::distinct(report_longitude) %>%
-  nrow()
+  nrow() # slight difference but ignore.
 
 duplicates = nc_sightings %>% ##inspecting duplicate longitudes and they are seemingly all different sightings 
   dplyr::group_by(report_longitude) %>%
   dplyr::filter(dplyr::n() > 1)
 
 ##~~summarize species data~~##
-summary_sightings = nc_sightings %>%
+nc_sightings %>%
   dplyr::group_by(sighting_year, species_name) %>%
   dplyr::summarise(count = dplyr::n(), .groups = "drop")
+
+##~~summarize source~~##
+nc_sightings %>%
+  dplyr::group_by(report_source_entity) %>%
+  dplyr::summarise(count = dplyr::n(), .groups = "drop")
+
 
 
 ##map sightings
@@ -102,23 +116,18 @@ leaflet::leaflet() %>%
 
 ##~~~~~~~~~~~~~Alerts~~~~~~~~~~~~~~~~##
 
-nc_alerts = main_dataset %>%
-  dplyr::filter(
-    report_created_at >= start_date,
-    report_created_at <= end_date) %>%  
-  sf::st_as_sf(coords = c("report_longitude", "report_latitude"), crs = 4326, remove = FALSE) %>% 
-  sf::st_filter(northcoast)
+nc_alerts = main_dataset %>% 
+  dplyr::filter(sighting_id %in% nc_sightings$sighting_id)
+
 
 nc_alerts_unique = alerts_main %>% 
-  dplyr::filter(
-    alert_created_at >= start_date,
-    alert_created_at <= end_date,
-    !is.na(report_longitude))%>% 
-  sf::st_as_sf(coords = c("report_longitude", "report_latitude"), crs = 4326, remove = FALSE) %>% 
-  sf::st_filter(northcoast)
+  dplyr::filter(sighting_id %in% nc_sightings$sighting_id) %>% 
+  dplyr::mutate(latitude = report_latitude,
+                longitude = report_longitude)
 
-##in the northcoast, 2356 alerts sent - but we need context on ZOI based alerts 
+##in the northcoast, 2,230 alerts sent - but we need context on ZOI based alerts 
 ##QUESTION - why is nc_alerts the same alerts as from nc_alerts_unique isn't alerts_main set for unique alerts only? main_dataset is just a total (inflated) number?
+## ANSWER ---- FILTER USING THE SIGHTING_ID FROM YOUR SIGHTINGS LIST TO HAVE ONE SOURCE OF TRUTH.
 
 ##context filtering to establish that alerts may appear inflated in ashley's map. This is due to ZOI notifications. 
 summary = nc_alerts_unique %>%
@@ -157,7 +166,7 @@ leaflet::leaflet() %>%
     fillOpacity = 1)
 
 ##save spreadsheet of sightings
-writexl::write_xlsx(nc_sightings, "C:/Users/CarlyGreen/OneDrive - Ocean Wise Conservation Association/Whales Initiative - Data Requests/Ashley Bachert/Ashley Bachert - North Coast/PRPA March 2026 Request/2026-03-23-NCsightings1.xlsx")
-###even after all of this there are still some clear duplicates based of exact sighting time stamps as well as some that are dulicates base off of .. vibes. Similar times, Similar lat/long, same observer. 
+# writexl::write_xlsx(nc_sightings, "C:/Users/CarlyGreen/OneDrive - Ocean Wise Conservation Association/Whales Initiative - Data Requests/Ashley Bachert/Ashley Bachert - North Coast/PRPA March 2026 Request/2026-03-23-NCsightings1.xlsx")
 ##Question: how can we combat this without manually going through? or do we need to manually go through..
 ##Question: Are we only to provide 'approved' sightings in this case? There were many summer sightings left 'waiting review' because we were waiting for databse cleaning to happen before processing all of these duplicates. 
+## ANSWER - CONTEXT DEPEDANT. IN THIS CASE PRPA IS INTERESTED IN DATA IN + DATA OUT, NO QA'ED SIGHTINGS FOR A PAPER. ALL REPORT STATUS OTHER THAN REJECTED IS FINE. 
