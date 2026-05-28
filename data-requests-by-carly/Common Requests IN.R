1eee2455174c2ee5998785f3f437613c96cbf4ae
CarlyGreen <carly.green@ocean.org>
2026-05-28 16:19:42 -0300

    Parameter set up code

--- data-requests-by-carly/Common Requests IN.R
+++ data-requests-by-carly/Common Requests IN.R
@@ -5,267 +5,135 @@
 
 ####~~~~~~~~~~~~~~~~~~Step 1: Load Data Sources~~~~~~~~~~~~~~~~~~####
 
-source("whalereport-dahsboard/config.R")
-source(config.R())
+source("config.R")
+source("data-import.R")
+source("data-cleaning.R")
 
-####~~~~~~~~~~~~~Type 1: Request for Ocean Wise (unless other entities allowed) sightings in a defined area and date range~~~~~~~~~~####
+##Note that most data requests we are alerting sightings_main table for sightings, and alerts_main for alerts.
 
-##Step 1 upload the defined area of interest (aoi) 
-aoi = sf::st_read("C:/Users/CarlyGreen/OneDrive - Ocean Wise Conservation Association/Documents/Operations/RStudio/Data Requests/samplearea.geojson")
+####~~~~~~~~~~~~~~~~~~~~~~Step 2: Setting Parameters~~~~~~~~~~~~~~~~~~~~~~####
+  
+#### Date Range (Default is from Jan 1 1931 to today) 
+start_date = lubridate::as_date("1931-01-01")
+end_date = lubridate::today() 
+ ## Question - is it better to have as start_date = lubridate::ymd_hms("2025-04-01 00:00:00", tz = "America/Los_Angeles")
 
-##Step 2 map the boundary to ensure it looks correct
+#### Species Filter (use null for all species)
+species_filter = NULL
+### example species_filter = c("Killer whale")
+
+#### Source Entity Filter
+source_filter = NULL 
+### example species_filter = c("Ocean Wise Conservation Association", "Orca Network via Conserve.io app")
+
+#### add context about which source entities to include when*******
+## Generally, external data requests, just filter for OWCA as the report_source_entity
+
+#### Area Filter
+area_of_interest = NULL
+### example 
+area_of_interest = "C:/Users/CarlyGreen/OneDrive - Ocean Wise Conservation Association/Documents/Operations/RStudio/Data Requests/samplearea.geojson"
+
+if(!is.null(area_of_interest)) {
+  request_area = sf::st_read(area_of_interest)
+  
+  ## Check geometry
+  sf::st_crs(request_area)
+  
+  ## Transform if required to WGS84
+  request_area = sf::st_transform(request_area, 4326)
+}
+
+## OPTIONAL check the request_area 
 leaflet::leaflet() |> 
   leaflet::addTiles() |>  # or addProviderTiles(providers$CartoDB.Positron)
-  leaflet::addPolygons(data = aoi, 
+  leaflet::addPolygons(data = request_area, 
                        color = "red", 
                        fill = FALSE, 
                        weight = 2)
-##Step 3 check if you need to transform 
-sf::st_crs(aoi)
 
-##Step 4 (if needed) transform data to wgs84
-# aoi_wgs84 = sf::st_transform(aoi, 4326)
+####~~~~~~~~~~~~~~~~~~~~~~Step 3: Filter sightings_main table for sightings~~~~~~~~~~~~~~~~~~~~~~####
 
-
-##Step 5 create start and end dates (do I need the time zone still)
-start_date = lubridate::as_date("2023-01-30")
-end_date = lubridate::as_date("2026-01-30")
-
-
-###~~~Step 6.1 filter and clean sightings_main~~~###
-##filter and clean sightings within aoi
+#### SIGHTINGS MAIN TO SF 
 sightings_filtered = sightings_main |> 
-  sf::st_as_sf(coords = c("report_longitude", "report_latitude"), crs = 4326, remove = FALSE) |>
-  sf::st_filter(aoi) |>
-  sf::st_drop_geometry() |> 
-  
-  dplyr::mutate(
-    ##Convert everything to PST/PDT consistently and rounding by 15 minutes
-    sighting_date = lubridate::with_tz(sighting_date, tzone = "America/Los_Angeles"),
-    lat_rnd = round(report_latitude, 4),
-    lon_rnd = round(report_longitude, 4),
-    time_bucket = lubridate::round_date(sighting_date, "15 mins"),
-  ) |>
+  sf::st_as_sf(
+    coords = c("report_longitude", "report_latitude"), crs = 4326, remove = FALSE)
+
+sightings_filtered = sightings_filtered |> 
   dplyr::filter(
     sighting_date >= start_date,
-    sighting_date <= end_date
-  ) |> 
+    sighting_date <= end_date) 
+##Question at this point sightings main and sightings_filtered should be the same, sightings_main has about 100 more sightings.
 
-###~~~~6.2 optional filters~~~###
-  dplyr::filter(!report_status== "rejected") |>
-  ##Filter for OWCA only if required, which is required when providing data tables to external parties
-  dplyr::filter(report_source_entity == "Ocean Wise Conservation Association") |>  
-  # dplyr::filter(!report_source_entity == "Orca Network via Conserve.io app") |> 
-  ##for species specific filtering 
-  # dplyr::filter(species_name == "Killer whale") |> 
-  # dplyr::filter(ecotype_name != "Northern Resident" | is.na(ecotype_name)) |> 
+### SPECIES FILTER
+if(!is.null(species_filter)) {
+  sightings_filtered = sightings_filtered |> 
+    dplyr::filter(species_name %in% species_filter)
+}
 
-###~~~6.3 round time, clean confidence column name and remove duplicates~~~###
+### OPTIONAL ECOTYPE FILTER 
+# sightings_filtered = sightings_filtered |> 
+#   dplyr::filter(ecotype_name %in% c("Northern Resident", "Transient"))
+  # dplyr::filter(ecotype_name != "Northern Resident" | is.na(ecotype_name))
+
+
+### SOURCE ENTITY FILTER 
+if(!is.null(source_filter)) {
+  sightings_filtered = sightings_filtered |> 
+    dplyr::filter(report_source_entity %in% source_filter)
+}
+
+### AREA FILTER 
+if(!is.null(area_of_interest)) {
+  sightings_filtered = sightings_filtered |> 
+    sf::st_filter(request_area)
+}
+
+### REMOVE REJECTED 
+sightings_filtered = sightings_filtered |> 
+  dplyr::filter(
+    report_status %in% c("approved", "auto_approved", "on_review", "created", "waiting_review")
+  )
+
+###OPTIONAL ROUNDING TO CHECK FOR DUPLICATES 
+sightings_filtered = sightings_filtered |> 
+  dplyr::mutate(
+    ##Convert everything to PST/PDT consistently and rounding by 15 minutes
+    # sighting_date = lubridate::with_tz(sighting_date, tzone = "America/Los_Angeles"),
+    lat_rnd = round(report_latitude, 4),
+    lon_rnd = round(report_longitude, 4),
+    time_bucket = lubridate::round_date(sighting_date, "5 mins"),
+  ) 
+
+## Final cleaning and duplicate removal 
+sightings_filtered = sightings_filtered |> 
   dplyr::group_by(lat_rnd, lon_rnd, time_bucket) |> 
   dplyr::mutate(
     is_duplicate = dplyr::n() > 1
   ) |> 
-  dplyr::slice(1) |> 
+  dplyr::slice(1)
+
+## Tidying up arrangement and rename confidence column
+sightings_filtered = sightings_filtered |> 
   dplyr::ungroup() |> 
   dplyr::rename(confidence = observer_confidence) |> 
-  dplyr::arrange(sighting_date) 
+  dplyr::arrange(sighting_date) |> 
+  sf::st_drop_geometry()
+
+cat(sprintf("Records after date, species, source, + area filter: %d\n", nrow(sightings_filtered)))
 
 
-#Step 7 check for duplicates and inspect the data table 
+####~~~~~~~~~~~~~~~~~~~~~~Step 4: Filter alerts_main table for unique notifications~~~~~~~~~~~~~~~~~~~~~~####
 
-##checking for duplicates (can be based on longitude or latitude)
-sightings_filtered |>
-  dplyr::distinct(report_longitude) |>
-  nrow() 
+##use Alerts_main for unique notifications
+##use Main_dataset could work but it has more of the sighting columns too? 
 
-##create a table for inspecting the duplicates if needed
-duplicates = sightings_filtered
-dplyr::group_by(report_longitude) |>
-  dplyr::filter(dplyr::n() > 1)
+alerts_filtered = alerts_main |> 
+  dplyr::filter(
+    alert_created_at >= start_date,
+    alert_created_at <= end_date)
 
-##~~Depending on the request, you can summarize species data, source, etc.~~##
-sightings_filtered |>
-  dplyr::group_by(sighting_year, species_name) |>
-  dplyr::summarise(count = dplyr::n(), .groups = "drop")
+alerts_filtered = alerts_filtered |> 
+  dplyr::filter(sighting_id %in% filtered_sighting_ids)
 
-sightings_filtered |>
-  dplyr::group_by(report_source_entity) |>
-  dplyr::summarise(count = dplyr::n(), .groups = "drop")
-
-
-
-####~~~~~~~~~~Step 8 COMMENT CLEANING~~~~~~####
-####Question - do we need to add code in to format the historical sighting comments 
-
-
-##for Alex's reformatted comments to be cleaned 
-remove_fields = c(
-  "first_name",
-  "last_name",
-  "email",
-  "processed_by",
-  "how_you_heard_other",
-  "experience",
-  "why_in_water",
-  "when_in_water",
-  "how_you_heard"
-)
-
-pattern = paste0(
-  "\\|?\\s*(",
-  paste(remove_fields, collapse = "|"),
-  ")\\s*:[^|]*"
-)
-
-sightings_out = sightings_filtered |>
-  
-  dplyr::mutate(
-    
-    comments = dplyr::case_when(
-      
-      # If Original Comments exists,
-      # keep everything after it
-      stringr::str_detect(
-        comments,
-        stringr::regex(
-          "Original Comments:",
-          ignore_case = TRUE
-        )
-      ) ~ stringr::str_replace(
-        comments,
-        stringr::regex(
-          ".*Original Comments:\\s*",
-          ignore_case = TRUE
-        ),
-        ""
-      ),
-      
-      # Otherwise, if Historical Import exists,
-      # keep only "Historical Import"
-      stringr::str_detect(
-        comments,
-        stringr::regex(
-          "Historical Import",
-          ignore_case = TRUE
-        )
-      ) ~ "Historical Import",
-      
-      # Otherwise keep original comment
-      TRUE ~ comments
-      
-    ) |>
-      
-      # Remove selected metadata fields
-      stringr::str_remove_all(
-        stringr::regex(
-          pattern,
-          ignore_case = TRUE
-        )
-      ) |>
-      
-      # Remove any remaining email addresses
-      stringr::str_remove_all(
-        stringr::regex(
-          "[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}",
-          ignore_case = TRUE
-        )
-      ) |>
-      
-      # Remove phone numbers
-      stringr::str_remove_all(
-        stringr::regex(
-          "\\b\\(?\\d{3}\\)?[-. ]?\\d{3}[-. ]?\\d{4}\\b"
-        )
-      ) |>
-      
-      # Clean duplicated separators
-      stringr::str_replace_all("\\|\\s*\\|", "|") |>
-      
-      # Remove leading/trailing separators
-      stringr::str_replace_all("^\\s*\\|\\s*", "") |>
-      stringr::str_replace_all("\\s*\\|\\s*$", "") |>
-      
-      # Remove extra line breaks
-      stringr::str_replace_all("[\\r\\n]+", " ") |>
-      
-      # Squish repeated whitespace
-      stringr::str_squish()
-    
-  )
-
-
-###~~~Step 9 column Select~~~###
-
-sightings_out = sightings_out |>
-  dplyr::select(
-    sighting_id,
-    sighting_date,
-    report_status,
-    species_name,
-    species_scientific_name,
-    ecotype_name,
-    report_latitude,
-    report_longitude,
-    report_count,
-    count_type,
-    direction,
-    confidence,
-    behaviour,
-    comments,
-    sighting_platform_name,
-    report_source_entity,
-    report_source_type,
-    sighting_year,
-    sighting_month,
-  )
-
-cat(sprintf("Final output: %d rows x %d columns\n", nrow(sightings_out), ncol(sightings_out)))
-
-
-
-#Step 10 if needed, map it (if requested but also is good to check everything looks correct)
-#create as spatial data again
-sightings_filtered_sf = sightings_filtered |>
-  sf::st_as_sf(
-    coords = c("report_longitude", "report_latitude"),
-    crs = 4326,
-    remove = FALSE
-  )
-
-
-#map it
-leaflet::leaflet() |> 
-  leaflet::addProviderTiles(leaflet::providers$OpenStreetMap) |> 
-  # leaflet::addPolygons(
-  #   data = boundary,
-  #   color = "blue",
-  #   weight = 2,
-  #   fill = FALSE
-  # ) |> 
-  leaflet::addCircleMarkers(
-    data = sightings_filtered_sf,
-    radius = 4, 
-    stroke = TRUE,
-    weight = 1,
-    color = "#FFCE34",
-    fillColor = "#FFCE34",
-    fillOpacity = 1)
-
-
-
-####~~~~~~~~~Type 2: Running total of Sightings and Alerts sent ~~~~~~~~~~####
-####~~~~~~~~~~Type 3: Organizational specific sightings data request~~~~~~####
-
-##Or instead of formatting by request type, could format by parameters##
-
-##~~~Date Range~~~##
-start_date = lubridate::ymd_hms("yyyy-mm-dd 00:00:00", tz = "America/Los_Angeles")
-end_date   = lubridate::ymd_hms("yyyy-mm-dd 23:59:59", tz = "America/Los_Angeles")
-
-##~~~Species Selection~~~##
-
-##~~~Geography~~~##
-
-##~~~Data Source (i.e. organization, OWSN, autoamted sightings)~~~##
-
-##~~~Output (table and/or map)~~~##
+cat(sprintf("Records after date, species, source, + area filter: %d\n", nrow(alerts_filtered)))
