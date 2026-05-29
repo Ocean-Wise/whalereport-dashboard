daa765b01001e1bab61369bd779bfb3376bbbb21
CarlyGreen <carly.green@ocean.org>
2026-05-29 16:38:10 -0300

    first draft of commmon requests OUT

--- data-requests-by-carly/Common Requests OUT.R
+++ data-requests-by-carly/Common Requests OUT.R
@@ -4,10 +4,10 @@
 ##Common Request Outputs, investigating, visualizations
 ##Must run Common Requests IN first
 
-### run Common Requests IN (with all proper parameters filled in)
-source("Common Requests IN.R")
+### run Common Requests IN (with all proper parameters filled in or as is if no filters)
+# source("Common Requests IN.R")
 
-### ANALYSIS  - outputs from sightings_filtered
+####~~~~~~~~~~~~~~~~~~Sightings Outputs and Analysis~~~~~~~~~~~~~~~~~~####
 
 ### checking for duplicates (can be based on longitude or latitude)
 sightings_filtered |>
@@ -15,8 +15,9 @@ sightings_filtered |>
   nrow() 
 
 ### create a table for inspecting the duplicates if needed
+##QUESTION - how are there still rows marked 'as duplicate' TRUE if we removed them in Common Requests IN.R???
 duplicates = sightings_filtered |> 
-dplyr::group_by(report_longitude) |>
+dplyr::group_by(report_longitude, report_latitude) |>
   dplyr::filter(dplyr::n() > 1)
 
 ### Depending on the request, you can summarize species data, source, etc.
@@ -29,6 +30,7 @@ source_summary = sightings_filtered |>
   dplyr::summarise(count = dplyr::n(), .groups = "drop")
 
 ############### TEMPORARY FOR NOW - comment cleaning 
+## QUESTION - orca network commments always specify a name, but the likelyhood of us sharing orca network data is low anyways so it's ok right?
 
 remove_fields = c(
   "first_name",
@@ -124,7 +126,7 @@ sightings_out = sightings_filtered |>
     
   )
 
-##Column select
+### Column select
 
 sightings_out = sightings_out |>
   dplyr::select(
@@ -168,8 +170,59 @@ writexl::write_xlsx(
   ),
   path = paste0("C:/Users/",user,"/Downloads/",Sys.Date(),"-sightingsorg.xlsx"))
 
+####~~~~~~~~~~~~~~~~~Organization Specific Sightings~~~~~~~~~~~~~~~~~~~####
 
-### MAPPING SIGHTINGS 
+### Sightings from a specific organization (string detect org name, and/or use the email ending i.e. wsdot)
+## note common regex statements provided in sandbox. 
+# org_sightings = sightings_filtered |>
+#   dplyr::filter(
+#     sighting_date >= start_date,
+#     sighting_date <= end_date,
+#     stringr::str_detect(observer_organization,
+#                         stringr::regex("washington state ferries |wsf", ignore_case = TRUE)
+#   ) | stringr::str_detect(observer_email, "wsdot") #including any ws ferries organization name OR emails
+# )
+
+### need to column clean org_sightings
+# 
+# org_sightings = org_sightings |>
+#   dplyr::select(
+#     sighting_id,
+#     report_id,
+#     sighting_date,
+#     report_status,
+#     sighting_code,
+#     species_name,
+#     species_scientific_name,
+#     ecotype_name,
+#     report_latitude,
+#     report_longitude,
+#     report_count,
+#     count_type,
+#     direction,
+#     confidence,
+#     behaviour,
+#     comments,
+#     sighting_platform_name,
+#     report_source_entity,
+#     report_source_type,
+#     observer_name,
+#     observer_email,
+#     observer_organization,
+#     sighting_year,
+#     sighting_month,
+#   )
+
+### clean up organization name for consistency - change to name kept in Salesforce (i.e. Washington State Ferries)
+# org_sightings = org_sightings |> 
+#   dplyr::mutate(
+#     observer_organization = "ORGANIZATION NAME HERE")
+
+# ### save organization specific spreadsheet. 
+# openxlsx::write.xlsx(org_sightings, paste0("C:/Users/", user, "/Downloads/", Sys.Date(),"-organization_sightings.xlsx"))
+
+
+###~~~~~~~~~~~~~~~~~~Mapping Sightings~~~~~~~~~~~~~~~~~~~~~###
 
 ### create as spatial data
 sightings_filtered_sf = sightings_filtered |>
@@ -181,6 +234,8 @@ sightings_filtered_sf = sightings_filtered |>
 
 
 ### sightings map: general
+
+## QUESTION/note we need to remove/reject the test sightings because when I map alllll sightings I am seeing whales in Africa, Greenland etc.
 leaflet::leaflet() |> 
   leaflet::addProviderTiles(leaflet::providers$OpenStreetMap) |> 
   # leaflet::addPolygons(
@@ -254,45 +309,49 @@ leaflet::leaflet() %>%
     title = "Source",
   )
 
-####~~~~~~~~~~~~ALERTS~~~~~~~~~~~~~####
+###~~~~~~~~~~~~~~~~~~Alert Outputs and Analysis~~~~~~~~~~~~~~~~~~~~~###
 
-### ANALYSIS  - outputs from alerts_filtered
-
-
-### checking for duplicates QUESTION - by alert user id? do we need to do this?
+### checking for duplicates 
+##QUESTION - dplyr::distinct by alert user id? or what else would we use? also this may not be a necessary step 
 alerts_filtered |>
   dplyr::distinct(alert_user_id) |>
   nrow() 
 
 ### create a table for inspecting the duplicates if needed
-duplicates = sightings_filtered |> 
+duplicates_alerts = alerts_filtered |> 
   dplyr::group_by(alert_user_id) |>
   dplyr::filter(dplyr::n() > 1)
 
-### Depending on the request, you can summarize by organization email, context
+### Depending on the request, you can summarize by delivery method, context, organization
+##COMMENT - I Feel like there may be a better way to do this then how I did this.. 
 
-### summarizing the delivery methods 
-notification_types = alerts_filtered |>
-  tidyr::pivot_longer(
-    cols = c(email_sent, sms_sent, push_sent),
-    names_to = "method",
-    values_to = "sent"
-  ) |>
-  dplyr::filter(sent == TRUE)
-
-notification_types = notification_types |>
-  dplyr::mutate(
-    method_label = dplyr::case_when(
-      method == "email_sent" ~ "Email",
-      method == "sms_sent" ~ "SMS",
-      method == "push_sent" ~ "Push"
-    )
+### Summary of unique alerts by year 
+unique_alert_summary = alerts_filtered |> 
+  dplyr::group_by(alert_year) |> 
+  dplyr::summarize(
+    unique_alerts = dplyr::n_distinct(alert_id),
+    .groups = "drop"
   )
-### summary table for delivery methods
-notification_summary = notification_types |>
-  dplyr::group_by(alert_year, method_label) |>
+
+### How many people are receiving alerts (notifications) no matter what their delivery method is
+recipient_summary = alerts_filtered |>
+  dplyr::group_by(alert_year) |>
   dplyr::summarise(
-    count = dplyr::n(),
+    unique_recipients = dplyr::n_distinct(alert_user_id),
+    .groups = "drop"
+  )
+
+### how many notifications being sent (one person can receive up to three: sms, push, email)
+notifications_sent = alerts_filtered |> 
+  dplyr::group_by(alert_year) |>
+  dplyr::summarise(
+    total_email_sends = sum(email_sent == TRUE, na.rm = TRUE),
+    total_sms_sends = sum(sms_sent == TRUE, na.rm = TRUE),
+    total_push_sends = sum(push_sent == TRUE, na.rm = TRUE),
+    total_notification_sends =
+      total_email_sends +
+      total_sms_sends +
+      total_push_sends,
     .groups = "drop"
   )
 
@@ -309,16 +368,84 @@ notification_context = alerts_filtered |>
   dplyr::group_by(year = alert_year, context_label) |> 
   dplyr::summarize(count = dplyr:: n(), .groups = "drop")
 
-### summary of alerts sent to specific organiztion 
-org_notifications = 
+### summary of alerts sent to specific organization --- NEED SUPPORT ON THIS PIECE --------
+##COMMENT/QUESTION this was done with main_dataset over alerts_main because we are pulling user_email_recipient. I think this means we can't use alerts_filtered?
+##COMMENT alerts_filtered does have target_address_email but only if that person is receiving email notifs. SO I still think main_dataset is best yes?  
+org_alerts <- main_dataset |>
+  dplyr::filter(
+    stringr::str_detect(
+      user_organization_recipient,
+      stringr::regex("washington state ferries|wsf", ignore_case = TRUE) ##example regex
+    ) |
+      stringr::str_detect(observer_email, "wsdot"),
+    alert_year == 2025
+  ) |>
+  dplyr::group_by(user_email_recipient) |>
+  dplyr::summarise(
+    email_only = sum(email_sent & !sms_sent & !push_sent, na.rm = TRUE),
+    sms_only   = sum(sms_sent & !email_sent & !push_sent, na.rm = TRUE),
+    push_only  = sum(push_sent & !email_sent & !sms_sent, na.rm = TRUE),
+    
+    email_sms  = sum(email_sent & sms_sent & !push_sent, na.rm = TRUE),
+    email_push = sum(email_sent & push_sent & !sms_sent, na.rm = TRUE),
+    sms_push   = sum(sms_sent & push_sent & !email_sent, na.rm = TRUE),
+    
+    all_three  = sum(email_sent & sms_sent & push_sent, na.rm = TRUE),
+    total_alerts = dplyr::n(),
+    .groups = "drop"
+  ) |>
+  dplyr::arrange(desc(total_alerts))
+
+##########Another Option for this^  would be to create a function?################
 
 
-####~~~~~~~~~~~~~Type 1: Request for Ocean Wise (unless other entities allowed) sightings in a defined area and date range~~~~~~~~~~###
-####~~~~~~~~~Type 2: Running total of Sightings and Alerts sent ~~~~~~~~~~####
-####~~~~~~~~~~Type 3: Organizational specific sightings data request~~~~~~####
+##still need to create
 ####Fiscal Year comparison 
-####Yearly comparison calendar year 
-####Organization specific sightings and Alerts 
+####Yearly comparison with calendar year 
+####Organization specific alert -- need support 
+
+###~~~~~~~~~~~~~~~~~~Alert Mapping~~~~~~~~~~~~~~~~~~~~~###
+###TO be added - heat maps, source specific map, any other type of alert map? 
+
+## General Alert Map
+leaflet::leaflet() |> 
+  leaflet::addProviderTiles(leaflet::providers$OpenStreetMap) |> 
+  # leaflet::addPolygons(
+  #   data = request_area,
+  #   color = "#A8007E",
+  #   weight = 2,
+  #   fill = FALSE
+  # ) |> 
+  leaflet::addCircleMarkers(
+    data = alerts_filtered,
+    radius = 4, 
+    stroke = TRUE,
+    weight = 1,
+    color = "grey",
+    fillColor = "#FFCE34",
+    fillOpacity = 1)
 
 
+####~~~~~~~~~~~~~~~~~~~~~~~~~~SANDBOX~~~~~~~~~~~~~~~~~~~~~~~~~###
 
+## Washington State Ferries regex statement 
+stringr::str_detect(observer_organization,
+                    stringr::regex("washington state ferries |wsf", ignore_case = TRUE)
+                    ) | stringr::str_detect(observer_email, "wsdot") #including any ws ferries organization name OR emails
+
+## BC Ferries regex statement
+stringr::str_detect(observer_organization, 
+                    stringr::regex("bc\\s*fer", ignore_case = TRUE)
+                    ) | stringr::str_detect(observer_email, "bcferries") #including any bc ferries organizations OR bcferries emails
+
+
+## List of specific emails to get sightings from (if provided) such as for Skana people
+  dplyr::filter(observer_email %in% c(
+    "gary.sutton@ocean.org",
+    "michael.judson@ocean.org",
+    "ashleybachert@hotmail.com",
+    "chloe.robinson@ocean.org",
+    "hannah.trotman@ocean.org",
+    "hcrichards00@gmail.com",
+    "olivia.heintzman@ocean.org"
+  ))
